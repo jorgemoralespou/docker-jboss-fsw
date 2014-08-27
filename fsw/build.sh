99670903014cfe7d73feb0945becd400f3aa38ca
jorgemoralespou <jorgemoralespou@gmail.com>
2014-08-27 15:37:14 +0200

    FSW SwitchYard image

--- fsw/build.sh
+++ fsw/build.sh
@@ -1,17 +1,20 @@
+#!/bina/bash
+
+FSW_VERSION=6.0.0
 #
 # We delete intermediate containers to get minimal size
 #
-docker run -it -v /software --name fsw_installers jboss/fsw_installers:6.0.0 echo "Installers volume ready"
+docker run -it -v /software --name fsw_installers jboss/fsw_installers:${FSW_VERSION} echo "Installers volume ready"
 docker run -it --name "fsw_install" --volumes-from fsw_installers jboss/base /software/fsw/install-fsw.sh
 #
 # If you dont have the volume with the software, but have it locally, can build with (replace two lines above):
-# docker run -t -i --name "dtgov_install" -v /home/jmorales/repositories/jorgemoralespou/jboss-virtual-environments.git/docker-containers/docker/fsw/installers/software:/software jboss/base /software/install-fsw.sh
+# docker run -t -i --name "fsw_install" -v $(PATH_TO_FSW)/fsw/installers/software:/software jboss/base /software/install-fsw.sh
 #
 #
-docker commit -m "FSW installed" fsw_install jboss/fsw_base:6.0.0
-docker rm -f fsw_install
-docker rm -f fsw_installers
+docker commit -m "FSW installed" fsw_install jboss/fsw_base:${FSW_VERSION}
+docker rm -vf fsw_install
+docker rm -vf fsw_installers
 
 # Build the appropiate images
 echo "Creating the FSW image"
-docker build --rm -t jboss/fsw:6.0.0 .
+docker build --rm -t jboss/fsw:${FSW_VERSION} .
---
+++ switchyard/run.sh
@@ -0,0 +1,4 @@
+# You can add /input volume, /output volume, 8787 port mapping for debugging, and command line options like, "-c standalone-ha.xml" or "--debug"
+#
+#
+docker run -it --rm -p 8080:8080 -p 9990:9990 -p 9999:9999 jboss/fsw_sy:6.0.0
